48656e6756d9c932606bffc7b097f36e03d6252a
Aasiq ali <aasiqali2332@gmail.com>
2024-04-18 13:48:00 +0530

    comment added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sqlite_flutter_crud/Authtentication/login.dart';
+//aasiq ali
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
